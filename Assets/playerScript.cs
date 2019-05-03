06a6472ce625c553589ad450a51fabde69a06463
super-tux <valentin.spadt@gmail.com>
2019-05-03 16:56:09 +0200

    made player/trail colors dynamic

--- Assets/playerScript.cs
+++ Assets/playerScript.cs
@@ -8,11 +8,29 @@ public class playerScript : MonoBehaviour
     public float xSpeed = 1.0f;
 
     private HFTInput m_hftInput;
+    private HFTGamepad m_gamepad;
+    private TrailRenderer tr;
 
     // Use this for initialization
     void Start()
     {
         m_hftInput = GetComponent<HFTInput>();
+        m_gamepad = GetComponent<HFTGamepad>();
+
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.color = m_gamepad.color;
+
+        tr = GetComponent<TrailRenderer>();
+        tr.material = new Material(Shader.Find("Sprites/Default"));
+
+        // A simple 2 color gradient with a fixed alpha of 1.0f.
+        float alpha = 1.0f;
+        Gradient gradient = new Gradient();
+        gradient.SetKeys(
+            new GradientColorKey[] { new GradientColorKey(m_gamepad.color, 0.0f), new GradientColorKey(m_gamepad.color, 1.0f) },
+            new GradientAlphaKey[] { new GradientAlphaKey(alpha, 0.0f), new GradientAlphaKey(alpha, 1.0f) }
+        );
+        tr.colorGradient = gradient;
     }
 
     // Update is called once per frame
