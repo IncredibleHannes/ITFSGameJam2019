1c3912adb58258faea3207d128eb6aba9a6869de
super-tux <valentin.spadt@gmail.com>
2019-05-03 14:12:22 +0200

    added test player script

---
+++ Assets/ScrollingBG.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Done_BGScroller : MonoBehaviour
+{
+    public float scrollSpeed = 0.4f;
+    public float tileSizeZ = 1.0f;
+
+    private Vector3 startPosition;
+
+    void Start ()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update ()
+    {
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeZ);
+        transform.position = startPosition + Vector3.forward * newPosition;
+    }
+}
---
+++ Assets/playerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+    public float ySpeed = 4.0f;
+    public float xSpeed = 0.4f;
+
+    private HFTInput m_hftInput;
+
+    // Use this for initialization
+    void Start () {
+        m_hftInput = GetComponent<HFTInput>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dy = ySpeed * (m_hftInput.GetAxis("Horizontal") /* + Input.GetAxis("Vertical")*/) * Time.deltaTime;
+        float dx = xSpeed * Time.deltaTime;
+        transform.position = transform.position + new Vector3(dx, dy, 0.0f);
+    }
+}
