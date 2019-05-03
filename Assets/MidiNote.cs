a670a8272d2b76ce5e916ee08d7ff75c9b419c01
Johannes Hartmann <Johannes.Hartmann.Calw@googlemail.com>
2019-05-03 15:26:15 +0200

    add midi note spawner

---
+++ Assets/MidiNote.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MidiNote : MonoBehaviour
+{
+    public float scrollSpeed = 2.0f;
+
+    private Vector3 startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newPosition = Time.time * scrollSpeed;
+        transform.position = startPosition + Vector3.left * newPosition;
+    }
+}
---
+++ Assets/MidiNoteSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MidiNoteSpawner : MonoBehaviour
+{
+    public GameObject prefab;
+
+    void Start()
+    {
+        GameObject newObject = Instantiate(prefab, new Vector3(0, 0, -1), Quaternion.identity) as GameObject;
+        newObject.transform.localScale = new Vector3(30, 3, 1);
+        Instantiate(prefab, new Vector3(1, 1, -1), Quaternion.identity);
+        Instantiate(prefab, new Vector3(2, -1, -1), Quaternion.identity);
+        Instantiate(prefab, new Vector3(3, 0, -1), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
