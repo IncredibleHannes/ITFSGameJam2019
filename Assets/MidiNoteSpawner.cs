5da6e72f5b4a2dd6298957b8d8da5b75a8e221ca
Jonas Auer <jonas.auer.94@gmail.com>
2019-05-03 23:24:02 +0200

    Commit WIP

--- Assets/MidiNoteSpawner.cs
+++ Assets/MidiNoteSpawner.cs
@@ -38,19 +38,6 @@ public class MidiNoteSpawner : MonoBehaviour
                 CreateNote(note.Time * lengthScaler, note.NoteNumber, note.Length * lengthScaler);
             }
         }
-
-        // GameObject newObject = Instantiate(prefab, new Vector3(0, 0, -1), Quaternion.identity) as GameObject;
-        // newObject.transform.localScale = new Vector3(30, 3, 1);
-        // Instantiate(prefab, new Vector3(1, -0.3f, -1), Quaternion.identity);
-        // Instantiate(prefab, new Vector3(2, -1, -1), Quaternion.identity);
-        // Instantiate(prefab, new Vector3(3, -1.2f, -1), Quaternion.identity);
-        // Instantiate(prefab, new Vector3(4, -1, -1), Quaternion.identity);
-        // Instantiate(prefab, new Vector3(5, -1.5f, -1), Quaternion.identity);
-        // Instantiate(prefab, new Vector3(6, -2, -1), Quaternion.identity);
-        // GameObject newObject2 = Instantiate(prefab, new Vector3(7, -2.5f, -1), Quaternion.identity) as GameObject;
-        // newObject2.transform.localScale = new Vector3(30, 3, 1);
-        // Instantiate(prefab, new Vector3(8, 1, -1), Quaternion.identity);
-        // Instantiate(prefab, new Vector3(9.3f, 1.8f, -1), Quaternion.identity);
     }
 
     IEnumerable<NotesCollection> LoadMidi(byte[] bytes)
