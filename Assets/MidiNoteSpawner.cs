27bdb79e77be8cbd01789c26039639b4cc8232c4
Johannes Hartmann <Johannes.Hartmann.Calw@googlemail.com>
2019-05-03 16:02:58 +0200

    add physic to player

--- Assets/MidiNoteSpawner.cs
+++ Assets/MidiNoteSpawner.cs
@@ -10,9 +10,14 @@ public class MidiNoteSpawner : MonoBehaviour
     {
         GameObject newObject = Instantiate(prefab, new Vector3(0, 0, -1), Quaternion.identity) as GameObject;
         newObject.transform.localScale = new Vector3(30, 3, 1);
-        Instantiate(prefab, new Vector3(1, 1, -1), Quaternion.identity);
+        Instantiate(prefab, new Vector3(1, -0.3f, -1), Quaternion.identity);
         Instantiate(prefab, new Vector3(2, -1, -1), Quaternion.identity);
-        Instantiate(prefab, new Vector3(3, 0, -1), Quaternion.identity);
+        Instantiate(prefab, new Vector3(3, -1.2f, -1), Quaternion.identity);
+        Instantiate(prefab, new Vector3(4, -1, -1), Quaternion.identity);
+        Instantiate(prefab, new Vector3(5, -1.5f, -1), Quaternion.identity);
+        Instantiate(prefab, new Vector3(6, -2, -1), Quaternion.identity);
+        GameObject newObject2 = Instantiate(prefab, new Vector3(7, -2.5f, -1), Quaternion.identity) as GameObject;
+        newObject2.transform.localScale = new Vector3(30, 3, 1);
     }
 
     // Update is called once per frame
